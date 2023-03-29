251f64558dcf7b6b545872326cdb571418020aa4
Indracit S <indrajithij802@gmail.com>
2023-03-29 14:05:15 +0530

    23329-205

--- routes/appRoutes.js
+++ routes/appRoutes.js
@@ -4,27 +4,27 @@ const router = express.Router()
 
 router.route('/dashboard')
         .get((req,res)=>{
-    res.render('dashboard',{title:'App.ir | Dashboard'})
+    res.render('dashboard',{title:'App.ir | Dashboard',username:req.session.username})
 })
     
 router.route('/reports')
         .get((req,res)=>{
-    res.render('reports',{title:'App.ir | Reports'})
+    res.render('reports',{title:'App.ir | Reports',username:req.session.username})
 })
 
 router.route('/monitoring')
         .get((req,res)=>{
-    res.render('monitoring',{title:'App.ir | Monitoring'})
+    res.render('monitoring',{title:'App.ir | Monitoring',username:req.session.username})
 })
 
 router.route('/syncdata')
         .get((req,res)=>{
-    res.render('syncdata',{title:'App.ir | SyncData'})
+    res.render('syncdata',{title:'App.ir | SyncData',username:req.session.username})
 })
 
 router.route('/searchdetails')
         .get((req,res)=>{
-    res.render('searchdetails',{title:'App.ir | SearchDetails'})
+    res.render('searchdetails',{title:'App.ir | SearchDetails',username:req.session.username})
 })
 
 module.exports = router
